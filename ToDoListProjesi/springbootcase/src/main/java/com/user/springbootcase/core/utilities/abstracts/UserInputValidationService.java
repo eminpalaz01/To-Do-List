232a4ced63f195d652ef86e1d95f51d845a599c7
emin10101 <emin.berke@hotmail.com>
2023-08-14 04:52:47 +0300

    Delete all

--- ToDoListProjesi/springbootcase/src/main/java/com/user/springbootcase/core/utilities/abstracts/UserInputValidationService.java
+++
@@ -1,7 +0,0 @@
-package com.user.springbootcase.core.utilities.abstracts;
-
-public interface UserInputValidationService {
-
-	public boolean usernameCheck(String username);
-	public boolean passwordCheck(String password);
-}
--- ToDoListProjesi/springbootcase/src/main/java/com/user/springbootcase/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.user.springbootcase.enums;
-
-public enum Role {
-
-	USER,ADMIN;
-}
